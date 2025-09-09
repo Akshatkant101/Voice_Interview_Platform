04d1b3d187a2edc8246007422f3b43d7048e8ea8
Akshatkant101 <akshatkant12@gmail.com>
2025-09-09 22:10:28 +0530

    Completed form pages sign in and sign up

--- components/Authform.tsx
+++ components/Authform.tsx
@@ -3,29 +3,43 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
-import {Form,FormControl,FormDescription,FormField,FormItem,FormLabel,FormMessage,} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import {Form} from "@/components/ui/form";
 import Image from "next/image";
 import Link from "next/link";
+import { toast } from "sonner";
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-});
+
+
+const authFormSchema=(type:FormType)=>{
+  return z.object({
+    name:type==='sign-up' ? z.string().min(3) : z.string().optional(),
+    email:z.string().email(),
+    password:z.string().min(3)
+  })
+}
 
 const Authform = ({type}:{type:FormType}) => {
-  // 1. Define your form.
+  const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      password: "",
     },
   });
 
-  // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+    try {
+      if (type==='sign-up') {
+        console.log("Sign up : ",values);
+      }else{
+        console.log("Sign in : ",values);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(`Here is your error:${error}`)
+    }
   }
   const isSignIn = type === 'sign-in'
   return (
@@ -33,7 +47,7 @@ const Authform = ({type}:{type:FormType}) => {
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
           <Image src="/logo.svg" alt="logo" width="38" height="32" />
-          <h2 className="text-primary-100">PrepWise</h2>
+          <h2 className="text-primary-100">Questly.ai</h2>
         </div>
         <h3>Practise job interview with AI</h3>
         <Form {...form}>
